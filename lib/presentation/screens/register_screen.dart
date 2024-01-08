250f44786ed4f21d245876048814ba29719ed308
ariel.duarte <arielpy@gmail.com>
2024-01-07 23:32:17 -0300

    Added form validator and onchange handler

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -43,24 +43,98 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisteForm extends StatelessWidget {
+class _RegisteForm extends StatefulWidget {
   const _RegisteForm({super.key});
+
+  @override
+  State<_RegisteForm> createState() => _RegisteFormState();
+}
+
+class _RegisteFormState extends State<_RegisteForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  String username = '';
+  String password = '';
+  String email = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
+        key: _formKey,
         child: Column(
-      children: [
-        const CustomTextFormField(
-            label: 'Name', hint: 'Enter your name', icon: Icons.person),
-        const SizedBox(
-          height: 20,
-        ),
-        FilledButton.tonalIcon(
-            onPressed: () {},
-            icon: const Icon(Icons.save),
-            label: const Text('Create User')),
-      ],
-    ));
+          children: [
+            CustomTextFormField(
+                onChanged: (value) => username = value,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an user name';
+                  }
+                  if (value.trim().isEmpty) {
+                    return 'User name is a required field';
+                  }
+                  if (value.length < 6) {
+                    return 'Require greater than 6 characters';
+                  }
+                  return null;
+                },
+                label: 'User Name',
+                hint: 'Enter an user name',
+                icon: Icons.person),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomTextFormField(
+                onChanged: (value) => email = value,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an email address';
+                  }
+                  if (value.trim().isEmpty) return 'Email is a required field';
+                  final emailRegExp = RegExp(
+                    r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+                  );
+                  if (!emailRegExp.hasMatch(value)) {
+                    return 'Invalid email address';
+                  }
+                  return null;
+                },
+                label: 'Email',
+                hint: 'Enter your email address',
+                icon: Icons.email),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomTextFormField(
+                onChanged: (value) => password = value,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                  if (value.trim().isEmpty) {
+                    return 'Password is a required field';
+                  }
+                  if (value.length < 6) {
+                    return 'Require greater than 6 characters';
+                  }
+                  return null;
+                },
+                obscureText: true,
+                label: 'Password',
+                hint: 'Enter password',
+                icon: Icons.password),
+            const SizedBox(
+              height: 20,
+            ),
+            FilledButton.tonalIcon(
+                onPressed: () {
+                  final isValid = _formKey.currentState!.validate();
+                  if (!isValid) return;
+                  print('Username: $username');
+                  print('Email: $email');
+                  print('Password: $password');
+                },
+                icon: const Icon(Icons.save),
+                label: const Text('Create User')),
+          ],
+        ));
   }
 }
